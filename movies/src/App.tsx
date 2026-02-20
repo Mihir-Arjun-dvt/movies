d7c5fa5d1378dc4a0e9beb4a275de588793d346a
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-20 17:11:11 +0200

    basic mock data added for movies and basic cards to display music

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,3 +1,4 @@
+import MovieCards from './components/MovieCards.tsx'
 import './index.css'
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-      
+        <MovieCards/>
       </div>
     </>
   )
