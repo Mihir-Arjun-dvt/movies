950d7dc8cfa62883354e539d2e60fef10651608b
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-20 17:31:26 +0200

    cards now render the movie data

--- movies/src/components/MovieCards.tsx
+++ movies/src/components/MovieCards.tsx
@@ -1,19 +1,27 @@
-export default function MovieCards() {
+interface MovieCardProps {
+    title: string;
+    genre: string;
+    showtime: string;
+    imageURL: string;
+}
+
+export default function MovieCards({ title, genre, showtime, imageURL }: MovieCardProps) {
     return (
         <div className="card bg-base-100 w-96 shadow-sm">
             <figure>
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes" />
+                    src={imageURL}
+                    alt={title}
+                    className="mt-4" />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    Card Title
-                    <div className="badge badge-secondary">NEW</div>
+                    {title}
+                    <div className="badge badge-secondary">{genre}</div>
                 </h2>
                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                 <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
+                    <div className="badge badge-outline">{showtime}</div>
                     <div className="badge badge-outline">Products</div>
                 </div>
             </div>
