cb0825438901a630aa0782cf4a542d39240271de
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-23 18:52:34 +0200

    adjusted styling on movie cards to have badges above title. This is for the hierarchy where movie showtimes and genre is more important than title. Especially when the poster is also provided but the main objective of the app is to show movie times and filter them accordingly

--- movies/src/components/MovieCards.tsx
+++ movies/src/components/MovieCards.tsx
@@ -14,18 +14,17 @@ export default function MovieCards({ title, genre, showtime, imageURL }: MovieCa
                 <img
                     src={imageURL}
                     alt={title}
-                    className="h-72 w-56 object-contain"
+                    className="h-72 w-56 object-contain mx-auto"
                 />
             </figure>
-
+            
             <div className="card-body bg-base-100 items-center text-center">
-                <h2 className="card-title text-lg">{title}</h2>
-
-                <div className="flex flex-wrap justify-center gap-2">
+                <div className="flex flex-wrap justify-center gap-2 mb-2">
                     <span className="badge badge-secondary">{genre}</span>
                     <MovieLabel showtime={showtime} />
                     <span className="badge badge-outline">{showtime}:00</span>
                 </div>
+                <h2 className="card-title text-lg">{title}</h2>
             </div>
         </article>
     );
